dd1d36d217658c627418719fe5033b524dc71e05
Administrator <Administrator@DESKTOP-TS23MN1>
2019-11-16 21:02:21 +0800

    AAA

--- src/main/java/com/asit/hello/HellWorldCtl.java
+++ src/main/java/com/asit/hello/HellWorldCtl.java
@@ -6,20 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller(value="/hello")
+@ResponseBody
+@Controller("/hello")
 public class HellWorldCtl {
 	@Resource(name="helloService")
 	HelloService service;
 	
-	@ResponseBody
-	@RequestMapping(name="/getMsg1")
+	@RequestMapping("/getMsg1")
 	public String getMsg1(){
 		return service.getMsg("getMsg1");
 	}
-	/*
-	@ResponseBody
-	@RequestMapping(name="/getMsg2")
+	@RequestMapping("/getMsg2")
 	public String getMsg2(){
 		return service.getMsg("getMsg2");
-	}*/
+	}
 }
